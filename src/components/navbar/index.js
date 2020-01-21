fcb659b7414add630fb4113550951cb9e7724cb2
joaoh <joao_camargo10@hotmail.com>
2020-01-21 20:07:40 -0300

    Dynamic menu (navbar)

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,29 +1,54 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
 
 import './navbar.css'
 
-const Navbar = props => (
-    <nav className="navbar navbar-expand-lg">
-        <span className="navbar-brand text-white font-weight-bold">City Events</span>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <i className="fas fa-bars text-white"></i>
-        </button>
+const Navbar = props => {
 
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav mr-auto">
-                <li className="nav-item active">
-                    <Link className="nav-link" to="/">Home</Link>
-                </li>
-                <li className="nav-item active">
-                    <Link className="nav-link" to="/register">Register</Link>
-                </li>
-                <li className="nav-item active">
-                    <Link className="nav-link" to="login">Login</Link>
-                </li>
-            </ul>
-        </div>
-    </nav>
-)
+    const dispatch = useDispatch()
+
+    return (
+        <nav className="navbar navbar-expand-lg">
+            <span className="navbar-brand text-white font-weight-bold">City Events</span>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <i className="fas fa-bars text-white"></i>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item active">
+                        <Link className="nav-link" to="/">Home</Link>
+                    </li>
+
+                    {
+                        useSelector(state => state.userLogged)
+                            ? <>
+                                <li className="nav-item active">
+                                    <Link className="nav-link" to="">Create Event</Link>
+                                </li>
+                                <li className="nav-item active">
+                                    <Link className="nav-link" to="">My Events</Link>
+                                </li>
+                                <li className="nav-item active">
+                                    <Link
+                                        onClick={() => dispatch({ type: 'LOG_OUT' })}
+                                        className="nav-link" >Logout</Link>
+                                </li>
+                            </>
+                            : <>
+                                <li className="nav-item active">
+                                    <Link className="nav-link" to="/register">Register</Link>
+                                </li>
+                                <li className="nav-item active">
+                                    <Link className="nav-link" to="login">Login</Link>
+                                </li>
+                            </>
+                    }
+                </ul>
+            </div>
+        </nav>
+    )
+}
 
 export default Navbar
